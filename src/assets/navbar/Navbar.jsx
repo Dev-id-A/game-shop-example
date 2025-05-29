5c12ad75e1e275421ed3affeaf256c2989e443b2
Dev-id-A <davidaranzana618@gmail.com>
2025-05-29 15:05:49 +0200

    navbar finished

--- src/assets/navbar/Navbar.jsx
+++ src/assets/navbar/Navbar.jsx
@@ -4,11 +4,13 @@ import "./navbar.css"
 function navbar() {
   return (
     <div id="navbar" className="d-flex justify-content-between">
-      
+
+        <a href="" id="nav-menu" className="col-3"><i class="bi bi-list"></i></a>
         <a href="" id="nav-home" className="col-3">Home</a>
         <a href="" id="nav-videogames" className="col-3">Videogames</a>
         <a href="" id="nav-accesories" className="col-3">Accesories</a>
         <a href="" id="nav-profile" className="col-3"><i class="bi bi-person-circle"></i></a>
+        <a href="" id="nav-cart" className="col-3"><i class="bi bi-cart4"></i></a>
 
     </div>
 
