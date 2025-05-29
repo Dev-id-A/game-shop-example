2767a62545204fd999113182709da9a25954b351
Dev-id-A <davidaranzana618@gmail.com>
2025-05-29 14:30:25 +0200

    navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './styles/App.css'
-import Navbar from './assets/navbar/navbar'
+import Navbar from './assets/navbar/Navbar.jsx'
 
 function App() {
   return (
-    <main>
-      <Navbar></Navbar>
+    <main id="main">
+      <Navbar/>
       <h1 id="title">Welcome to "The" Game Shop</h1>
     </main>
   )
--- src/assets/navbar/Navbar.jsx
+++ src/assets/navbar/Navbar.jsx
@@ -3,7 +3,15 @@ import "./navbar.css"
 
 function navbar() {
   return (
-    <div id="navbar"></div>
+    <div id="navbar" className="d-flex justify-content-between">
+      
+        <a href="" id="nav-home" className="col-3">Home</a>
+        <a href="" id="nav-videogames" className="col-3">Videogames</a>
+        <a href="" id="nav-accesories" className="col-3">Accesories</a>
+        <a href="" id="nav-profile" className="col-3"><i class="bi bi-person-circle"></i></a>
+
+    </div>
+
   )
 }
 
